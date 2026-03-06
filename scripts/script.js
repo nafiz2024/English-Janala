e86984bbdee9ee12524b65f65a7a53c2726fb211
Nafiz Alam <nafizalam.dev@gmail.com>
2026-03-07 00:08:43 +0600

    Dailog Lesson Section

--- scripts/script.js
+++ scripts/script.js
@@ -28,6 +28,77 @@ const removeActiveClass = () => {
     });
 }
 
+/* Word Details Modal */
+const loadWordDetail = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetail(details.data);
+
+     // Open the modal
+     const modal = document.getElementById('my_modal_1');
+     if (modal) {
+         modal.showModal();
+     }
+}
+
+const displayWordDetail = (word) => {
+    const dialog_container = document.getElementById('dialog_container');
+    dialog_container.innerHTML = "";
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <dialog id="my_modal_1" class="modal">
+        <div class="modal-box p-6">
+          <div class="border border-[#EDF7FF] rounded-xl p-6 space-y-8">
+            <h1 class="text-4xl text-[#000000] font-semibold leading-10 font_bangla">
+              ${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})
+            </h1>
+            <div class="space-y-2.5">
+              <h3 class="text-2xl text-[#000000] font-semibold leading-10">Meaning</h3>
+              <p class="text-2xl text-[#000000] font-medium leading-10">${word.meaning}</p>
+            </div>
+            <div class="space-y-2.5">
+              <h3 class="text-2xl text-[#000000] font-semibold leading-10">Example</h3>
+              <p class="text-2xl text-[#000000] font-medium leading-10">${word.sentence}</p>
+            </div>
+            <div class="space-y-2.5">
+              <h3 class="text-2xl text-[#000000] font-semibold leading-10 font_bangla">সমার্থক শব্দ গুলো</h3>
+              <div class="flex flex-wrap gap-5">
+
+                <button
+                  class="btn btn-soft btn-primary text-[#000000] hover:text-white"
+                >
+                  ${word.synonyms[0] ? word.synonyms[0] : 'সমার্থক শব্দ পাওয়া যায়নি'}
+                </button>
+                <button
+                  class="btn btn-soft btn-primary text-[#000000] hover:text-white"
+                >
+                    ${word.synonyms[1] ? word.synonyms[1] : 'সমার্থক শব্দ পাওয়া যায়নি'} 
+                </button>
+                <button
+                  class="btn btn-soft btn-primary text-[#000000] hover:text-white"
+                >
+                    ${word.synonyms[2] ? word.synonyms[2] : 'সমার্থক শব্দ পাওয়া যায়নি'}
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="modal-action">
+            <form method="dialog">
+              <button class="btn btn-primary rounded-3xl">
+                Complete Learning
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    `;
+    dialog_container.appendChild(div);
+}
+
 /* Word Section Function */
 const loadLevelWord = (level_no) => {
     const url = `https://openapi.programming-hero.com/api/level/${level_no}`;
@@ -81,7 +152,8 @@ const displayLevelWord = (words) => {
             "${word.meaning ? word.meaning : 'শব্দার্থ পাওয়া যায়নি '} / ${word.pronunciation ? word.pronunciation : 'Pronunciation পাওয়া যায়নি '}"
           </h1>
           <div class="flex justify-between mt-7 md:mt-14">
-            <button class="p-4 text-[#374957] text-lg bg-[#1A91FF]/10 hover:bg[#1A91FF]/20 rounded-lg">
+            <button onclick="loadWordDetail(${word.id})" 
+                    class="p-4 text-[#374957] text-lg bg-[#1A91FF]/10 hover:bg[#1A91FF]/20 rounded-lg">
               <i class="fa-solid fa-circle-info"></i>
             </button>
             <button class="p-4 text-[#374957] text-lg bg-[#1A91FF]/10 rounded-lg">
@@ -92,5 +164,7 @@ const displayLevelWord = (words) => {
             `;
         word_container.appendChild(div);
     });
+
+
 }
 loadLevels()
